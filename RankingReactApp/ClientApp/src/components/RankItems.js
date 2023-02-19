97e9ae76cfc2d9462a1a72582c3ec249ea60951b
Paul Wilson <konnectdigitalpro@gmail.com>
2023-02-19 11:57:12 +0800

    added drag and drop functions

--- RankingReactApp/ClientApp/src/components/RankItems.js
+++ RankingReactApp/ClientApp/src/components/RankItems.js
@@ -1,10 +1,36 @@
 ﻿import React, { useState, useEffect } from 'react';
 import MovieImageArr from './MovieImages.js';
+import RankingGrid from './RankingGrid.js';
 
 const RankItems = () => {
 
     const [items, setItems] = useState([])
     const dataType = 1;
+
+    function drag(ev) {
+        ev.dataTransfer.setData("text", ev.target.id);
+    }
+
+    function allowDrop(ev) {
+        ev.preventDefault();
+    }
+
+    function drop(ev) {
+
+        ev.preventDefault();
+        const targetElm = ev.target;
+        if (targetElm.nodeName === "IMG") {
+            return false;
+        }
+        if (targetElm.childNodes.length === 0) {
+            var data = parseInt(ev.dataTransfer.getData("text").substring(5));
+            const transformedCollection = items.map((item) => (item.id === parseInt(data)) ?
+                { ...item, ranking: parseInt(targetElm.id.substring(5)) } : { ...item, ranking: item.ranking });
+            setItems(transformedCollection);
+        }
+
+    }
+
 
     useEffect(() => {
         fetch(`item/${dataType}`)
@@ -18,11 +44,12 @@ const RankItems = () => {
 
     return (
         <main>
+            <RankingGrid items={items} imgArr={MovieImageArr} drag={drag} allowDrop={allowDrop} drop={drop}></RankingGrid>
             <div className="items-not-ranked">
             {
                     (items.length > 0) ? items.map((item) =>
                         <div className="unranked-cell">
-                            <img id={`item-${item.id}`} src={MovieImageArr.find(o => o.id === item.imageId)?.image} />
+                            <img id={`item-${item.id}`} style={{ cursor: "pointer" }} draggable="true" onDragStart={drag} src={MovieImageArr.find(o => o.id === item.imageId)?.image} />
                         </div>                        
                     ) : <div>Loading...</div>
             }
