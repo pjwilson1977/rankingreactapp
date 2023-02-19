8a982098fb7d4896e8124f255b61214b72b610b0
Paul Wilson <konnectdigitalpro@gmail.com>
2023-02-19 10:17:42 +0800

    First commit, fetch data via model & controller, show images

---
+++ RankingReactApp/Models/ItemModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace RankingReactApp.Models
+{
+	public class ItemModel
+	{
+        // https://www.youtube.com/watch?v=4RKuyp_bOhY
+        public int Id { get; set; }
+
+		public string Title { get; set; }
+
+		public int ImageId { get; set; }
+
+		public int Ranking { get; set; }
+
+		public int ItemType { get; set; }
+		
+	}
+}
+
